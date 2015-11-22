4030bdf7632a3397d78d2c7229a61eebb11e3c9c
José Roldán Jiménez <nosoycomplicao@gmail.com>
2015-11-22 22:52:05 +0100

    update

--- Pod/Classes/JRSplitVC.h
+++ Pod/Classes/JRSplitVC.h
@@ -10,13 +10,34 @@
 
 
 @interface JRSplitVC : UISplitViewController
+@property (strong, nonatomic) UIGestureRecognizer *longpres;
+@property NSString *storyboardFile;
+@property NSString *primaryViewControllerCollapsed;
+@property NSString *primaryViewControllerExpand;
+/**
+ *  Contructor
+ *
+ *  @param storyBoard String with Storybard
+ *
+ *  @return new object
+ */
+-(instancetype) initWithStoryBoardID:(NSString*)storyBoard;
+
+-(instancetype) initWithStoryBoardID:(NSString*)storyBoard
+            withPrimaryViewCollapsed:(NSString*)primaryViewControllerCollapsed
+               withPrimaryViewExpand:(NSString*)primaryViewControllerExpand;
+
+- (void)changePreferredPrimaryColumnWidth;
 /**
  *  Insert a Button for change mode of detailview
  *
- *  @param value YES o NO
  */
-//-(void)insertDisplayModeButton:(BOOL)value;
 -(void)insertDisplayModeButtonAnimated:(BOOL)animated;
+/**
+ *  Extract a Button dans SplitviewController
+ *
+ */
 -(void)extractDisplayModeButtonAnimated:(BOOL)animated;
+
 @end
 
--- Pod/Classes/JRSplitVC.m
+++ Pod/Classes/JRSplitVC.m
@@ -7,63 +7,86 @@
 //
 
 #import "JRSplitVC.h"
+
+
 #import "UIGestureRecognizer+functions.h"
 
 
 @interface JRSplitVC () <UISplitViewControllerDelegate>
-@property (strong, nonatomic) UIGestureRecognizer *longpres;
+
+
 
 @end
 
 @implementation JRSplitVC
-#pragma mark -
-#pragma mark - lifecycle
-#pragma mark -
+-(instancetype) initWithStoryBoardID:(NSString*)storyBoard
+{
+    self = [super init];
+    if (self) {
+        self.storyboardFile = storyBoard;
+    }
+    return self;
+
+}
+-(instancetype) initWithStoryBoardID:(NSString*)storyBoard
+            withPrimaryViewCollapsed:(NSString*)primaryViewControllerCollapsed
+               withPrimaryViewExpand:(NSString*)primaryViewControllerExpand
+{
+    self = [super init];
+    if (self) {
+        self.storyboardFile = storyBoard;
+        self.primaryViewControllerCollapsed = primaryViewControllerCollapsed;
+        self.primaryViewControllerExpand = primaryViewControllerExpand;
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self setDelegate:self];
-
+    [self setPresentsWithGesture:YES];
     [self setPreferredPrimaryColumnWidthFraction:0.5];
     [self setMaximumPrimaryColumnWidth:self.view.frame.size.width];
     [self setMinimumPrimaryColumnWidth:0];
     [self setPreferredDisplayMode:UISplitViewControllerDisplayModeAllVisible];
     
-//    UIViewController *vc = [[UIViewController alloc] init];
-//    UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
-//    [self.splitViewController showDetailViewController:nc
-//                                                sender:self];
+    UIViewController *vc = [[UIViewController alloc] init];
+    UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
+    [self.splitViewController showDetailViewController:nc
+                                                sender:self];
+    [self addObserver:self
+           forKeyPath:@"longpres"
+              options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
+              context:nil];
+    
+    
+//    [self setMaximumPrimaryColumnWidth:self.view.frame.size.width];
 
-    /**
-     * Capture gesture
-     */
     _longpres = [[UILongPressGestureRecognizer alloc]initWithTarget:self
-                                                       action:@selector(longPres)];
-
+                                                       action:@selector(changePreferredPrimaryColumnWidth)];
+//    [_longpres setMinimumNumberOfTouches:@0];
     if ([_longpres respondsToSelector:@selector(setMinimumNumberOfTouches:)]) {
         [_longpres setValue:0 forKey:@"numberOfTouchesRequired"];
     }
     [self.view addGestureRecognizer:_longpres];
-
-
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)longPres
+- (void)changePreferredPrimaryColumnWidth
 {
     CGFloat f = [_longpres fractionTouch];
     [self setPreferredPrimaryColumnWidthFraction:f];
    
 }
 
-#pragma mark -
-#pragma mark - UIViewController
-#pragma mark -
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
 - (void)viewWillLayoutSubviews
 {
     [super viewWillLayoutSubviews];
@@ -80,17 +103,22 @@
 
 }
 
-#pragma mark -
-#pragma mark - UISplitViewControllerDelegate
-#pragma mark -
-//- (UISplitViewControllerDisplayMode)targetDisplayModeForActionInSplitViewController:(UISplitViewController *)svc
-//{
-//
-//    return UISplitViewControllerDisplayModeAllVisible;
-//}
-#pragma mark -
-#pragma mark - UIBarButton Action
-#pragma mark -
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary *)change
+                      context:(void *)context
+{
+     NSLog(@"%@",keyPath);
+    if ([keyPath isEqualToString:@"longpres"]) {
+        UIGestureRecognizer * old  = [change valueForKey:@"old"];
+        UIGestureRecognizer * new  = change[@"new"];
+        [self.view removeGestureRecognizer:old];
+        [self.view addGestureRecognizer:new];
+    }
+}
+
+//Funciona y está bastante guapo a mi gusto
+// ¿no seria mejor idea insertar este método como una categoria?
 -(void)insertDisplayModeButtonAnimated:(BOOL)animated
 {
     if ([self.viewControllers count]>1) {
@@ -108,8 +136,7 @@
 
     }
 }
--(void)extractDisplayModeButtonAnimated:(BOOL)animated
-{
+-(void)extractDisplayModeButtonAnimated:(BOOL)animated{
     if ([self.viewControllers count]>1) {
         UINavigationController *nc = [self.viewControllers objectAtIndex:[@1 integerValue]];
         if (nc) {
@@ -125,5 +152,49 @@
     }
     
 }
+#pragma mark - UISplitViewControllerDelegate
+//- (UISplitViewControllerDisplayMode)targetDisplayModeForActionInSplitViewController:(UISplitViewController *)svc
+//{
+//
+//    return UISplitViewControllerDisplayModeAllVisible;
+//}
+    - (BOOL)splitViewController:(UISplitViewController *)splitViewController
+collapseSecondaryViewController:(UIViewController *)secondaryViewController
+      ontoPrimaryViewController:(UIViewController *)primaryViewController
+{
+    return YES;
+}
+-(UIViewController *)primaryViewControllerForCollapsingSplitViewController:(UISplitViewController *)splitViewController
+{
+    if (self.storyboardFile&& self.primaryViewControllerCollapsed) {
+        UIStoryboard *story = [UIStoryboard storyboardWithName:self.storyboardFile
+                                                        bundle:[NSBundle mainBundle]];
+        if (story) {
+            UIViewController *vc = [story instantiateViewControllerWithIdentifier:self.primaryViewControllerCollapsed];
+            UINavigationController *nc = [UINavigationController new];
+            [nc addChildViewController:vc];
+            return nc;
+        }
 
+    }else{
+        return  [UIViewController new];
+    }
+}
+- (UIViewController *)primaryViewControllerForExpandingSplitViewController:(UISplitViewController *)splitViewController
+{
+    if (self.storyboardFile&& self.primaryViewControllerExpand) {
+        
+        UIStoryboard *story = [UIStoryboard storyboardWithName:self.storyboardFile
+                                                        bundle:[NSBundle mainBundle]];
+        if (story) {
+            UIViewController *vc = [story instantiateViewControllerWithIdentifier:self.primaryViewControllerExpand];
+            UINavigationController *nc = [UINavigationController new];
+            [nc addChildViewController:vc];
+            return nc;
+        }
+
+    }else{
+        return [UIViewController new];
+    }
+}
 @end
